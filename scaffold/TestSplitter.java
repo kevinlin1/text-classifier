ed21877f1f36ff47c337c1a1b461f1ce4f86bd16
Kevin Lin <kevinl@cs.uw.edu>
2022-01-02 12:58:31 -0800

    scaffold: Rename RandomSplitter -> TestSplitter

--- scaffold/TestSplitter.java
+++ scaffold/TestSplitter.java
@@ -3,7 +3,7 @@ import java.util.function.*;
 import java.util.stream.*;
 
 // Computes a random split for the given data if the target depth has not been reached.
-public class RandomSplitter implements Splitter {
+public class TestSplitter implements Splitter {
     private double[][] matrix;
     private boolean[] labels;
     private Random random;
@@ -12,18 +12,18 @@ public class RandomSplitter implements Splitter {
     // The default value for the depth.
     private static final int DEFAULT_DEPTH = 2;
 
-    // Constructs a new RandomSplitter with the given design matrix and labels.
-    public RandomSplitter(double[][] matrix, boolean[] labels) {
+    // Constructs a new TestSplitter with the given design matrix and labels.
+    public TestSplitter(double[][] matrix, boolean[] labels) {
         this(matrix, labels, new Random());
     }
 
-    // Constructs a new RandomSplitter with the given design matrix, labels, and randomness.
-    public RandomSplitter(double[][] matrix, boolean[] labels, Random random) {
+    // Constructs a new TestSplitter with the given design matrix, labels, and randomness.
+    public TestSplitter(double[][] matrix, boolean[] labels, Random random) {
         this(matrix, labels, random, DEFAULT_DEPTH);
     }
 
-    // Constructs a new RandomSplitter with the given design matrix, labels, randomness, and depth.
-    public RandomSplitter(double[][] matrix, boolean[] labels, Random random, int depth) {
+    // Constructs a new TestSplitter with the given design matrix, labels, randomness, and depth.
+    public TestSplitter(double[][] matrix, boolean[] labels, Random random, int depth) {
         if (matrix.length != labels.length) {
             throw new IllegalArgumentException("matrix length != labels length");
         }
@@ -46,8 +46,8 @@ public class RandomSplitter implements Splitter {
         return new Splitter.Result(index, threshold, mask(left), mask(right));
     }
 
-    // Returns a new RandomSplitter containing only data where indices are true for the given predicate.
-    private RandomSplitter mask(IntPredicate predicate) {
+    // Returns a new TestSplitter containing only data where indices are true for the given predicate.
+    private TestSplitter mask(IntPredicate predicate) {
         int[] indices = IntStream.range(0, size()).filter(predicate).toArray();
         double[][] newMatrix = new double[indices.length][];
         boolean[] newLabels = new boolean[indices.length];
@@ -55,7 +55,7 @@ public class RandomSplitter implements Splitter {
             newMatrix[i] = matrix[indices[i]];
             newLabels[i] = labels[indices[i]];
         }
-        return new RandomSplitter(newMatrix, newLabels, random, depth - 1);
+        return new TestSplitter(newMatrix, newLabels, random, depth - 1);
     }
 
     // Returns the majority label for this splitter or false if the size() is 0.
